1a32eb0ad0b845aab757b9322b9274f3c20c6e0f
KxllSwxtch <dimashin9@gmail.com>
2023-03-30 15:50:36 +0900

    added basic collapse functionality

--- main.js
+++ main.js
@@ -10,9 +10,48 @@ $(document).ready(function () {
 })
 
 // KO MODEL
+function CategoryModel({ title, subtitle, elements }) {
+	const self = this
+
+	self.title = title
+	self.subtitle = subtitle
+	self.elements = elements
+
+	self.isCollapsed = ko.observable(false)
+	self.collapse = function () {
+		self.isCollapsed(!self.isCollapsed())
+	}
+}
+
 function ViewModel() {
 	const self = this
 
+	// Categories
+	self.categories = ko.observableArray([
+		new CategoryModel({
+			title: 'Обязательные для всех',
+			subtitle: 'Документы, обязательные для всех сотрудников без исключения',
+			elements: ko.observableArray([
+				{ title: 'Паспорт', status: 'Обязательный', subtitle: 'Для всех' },
+				{ title: 'ИНН', status: 'Обязательный', subtitle: 'Для всех' },
+			]),
+		}),
+
+		new CategoryModel({
+			title: 'Обязательные для трудоустройства',
+			subtitle:
+				'Документы, без которых невозможно трудоустройство человека на какую бы то ни было должность в компании вне зависимости от гражданства',
+			elements: ko.observableArray([]),
+		}),
+
+		new CategoryModel({
+			title: 'Специальные',
+			subtitle: '',
+			elements: ko.observableArray([]),
+		}),
+	])
+
+	// Single elements
 	self.singleCategories = ko.observableArray([
 		{
 			title: 'Тестовое задание кандидата',
@@ -28,29 +67,32 @@ function ViewModel() {
 			subtitle: '',
 		},
 	])
-
-	self.draggingItem = ko.observable(null)
-
-	self.startDrag = function (item) {
-		self.draggingItem(item)
-	}
-
-	self.afterMove = function (arg) {
-		const item = self.draggingItem()
-		const newIndex = arg.targetIndex
-		self.singleCategories.remove(item)
-		self.singleCategories.splice(newIndex, 0, item)
-		self.draggingItem(null)
-	}
-
-	self.sortableOptions = {
-		data: self.singleCategories,
-		beforeMove: function (arg) {
-			const item = arg.item
-			return item == self.draggingItem()
-		},
-		afterMove: self.afterMove,
-	}
 }
 
 ko.applyBindings(new ViewModel())
+
+/////
+///////// IN CASE IF I AM NOT ALLOWED TO USE JQUERY AND PLUGINS
+/////
+// self.draggingItem = ko.observable(null)
+
+// self.startDrag = function (item) {
+// 	self.draggingItem(item)
+// }
+
+// self.afterMove = function (arg) {
+// 	const item = self.draggingItem()
+// 	const newIndex = arg.targetIndex
+// 	self.singleCategories.remove(item)
+// 	self.singleCategories.splice(newIndex, 0, item)
+// 	self.draggingItem(null)
+// }
+
+// self.sortableOptions = {
+// 	data: self.singleCategories,
+// 	beforeMove: function (arg) {
+// 		const item = arg.item
+// 		return item == self.draggingItem()
+// 	},
+// 	afterMove: self.afterMove,
+// }
