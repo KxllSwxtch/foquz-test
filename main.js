b5788e5803542760fa89a508bf6faf8a996a7e39
KxllSwxtch <dimashin9@gmail.com>
2023-03-30 12:02:46 +0900

    added dragging functionality

--- main.js
+++ main.js
@@ -9,6 +9,48 @@ $(document).ready(function () {
 	})
 })
 
-function ViewModel() {}
+// KO MODEL
+function ViewModel() {
+	const self = this
+
+	self.singleCategories = ko.observableArray([
+		{
+			title: 'Тестовое задание кандидата',
+			subtitle:
+				'Россия, Белорусия, Украина, администратор филиала, повар-сушист, повар-пиццмейкер, повар горячего цеха',
+		},
+		{
+			title: 'Трудовой договор',
+			subtitle: '',
+		},
+		{
+			title: 'Мед. книжка',
+			subtitle: '',
+		},
+	])
+
+	self.draggingItem = ko.observable(null)
+
+	self.startDrag = function (item) {
+		self.draggingItem(item)
+	}
+
+	self.afterMove = function (arg) {
+		const item = self.draggingItem()
+		const newIndex = arg.targetIndex
+		self.singleCategories.remove(item)
+		self.singleCategories.splice(newIndex, 0, item)
+		self.draggingItem(null)
+	}
+
+	self.sortableOptions = {
+		data: self.singleCategories,
+		beforeMove: function (arg) {
+			const item = arg.item
+			return item == self.draggingItem()
+		},
+		afterMove: self.afterMove,
+	}
+}
 
 ko.applyBindings(new ViewModel())
